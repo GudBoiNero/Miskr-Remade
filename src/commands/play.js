75a56438a755e0e5bdad2aa3ef502d422c00645a
Ben Champagne <donkeykong797@gmail.com>
2023-06-07 13:54:22 -0400

    working on refactoring play.js for playlists

--- src/commands/play.js
+++ src/commands/play.js
@@ -4,6 +4,7 @@ const Globals = require("../globals.js");
 const GuildPlayer = require("../classes/GuildPlayer.js");
 const Track = require('../classes/Track.js')
 const ytdl = require('ytdl-core')
+const ytpl = require('ytpl')
 const ytsr = require('ytsr')
 const path = require('path')
 const fs = require('fs');
@@ -45,24 +46,66 @@ module.exports = {
 
         if (!connection) return
 
-        // Download
-        const results = await ytsr(query, { "pages": 1 })
-        
-        const result = results.items[0]
+        //#region video code rework
 
-        if (!result ? true : !result.id) {
+        // Get all video ids.
+        const videoIds = await (async() => {
+            const videoIds = []
+            // Use ytsr to determine whether or not the result is a video or playlist and grab the ids based on the res
+            const results = await (async () => {try { return await ytsr(query, { "pages": 1 })} catch {}})()
+            const result = results?.items[0]
+
+            if (!result) return
+
+            let resType = result.type
+            if (resType == "video") {
+                videoIds.push(result.id)
+            } 
+            else if (resType == "playlist") {
+                // Get each individual ID for each entry in a playlist
+                const playlistVideos = await ytpl(results.originalQuery)
+                playlistVideos.items?.forEach(video => videoIds.push(video.id))
+            }
+            return videoIds
+        })()
+
+        if (videoIds.length === 0) {
+            return await interaction.editReply({ embeds: [createThemedEmbed("Error", 'Could not find a video', 'Error')]})
+        };
+
+        videoIds.forEach(id => {
+            // Check if we already downloaded it
+
+            // Skip downloading if we have
+            
+        })
+
+
+        //#endregion
+
+        /*
+        //#region old video play code
+        // Download
+        const results = await (async () => {
+            try { return await ytsr(query, { "pages": 1 })} catch {}
+        })()
+        const result = results?.items[0]
+
+        console.log(results)
+
+        if (!result ?? (!result?.id || !result?.playlistID)) {
             return await interaction.editReply({embeds: [createThemedEmbed("Error", 'Could not find a video', 'Error')]})
         }
 
         const filePath = path.join(dlPath, `${result.id}.ogg`)
         const fileUrl = validVideoUrl.replace('__id__', result.id)
 
-        let cont = async () => {
+        const startPlayer = async () => {
             // If there is already a GuildPlayer with an active connection.
             // Add the track to the queue
-            let gp = Globals.getPlayer(guildId)
-            if ((gp ? gp.connection != undefined : false) && !gp.destroyed) {
-                gp.queue.addTrack(new Track(filePath, result))
+            const curGuildPlayer = Globals.getPlayer(guildId)
+            if ((curGuildPlayer ? curGuildPlayer.connection != undefined : false) && !curGuildPlayer.destroyed) {
+                curGuildPlayer.queue.addTrack(new Track(filePath, result))
 
                 if (!interaction.replied) {
                     return await interaction.editReply({embeds: [createThemedEmbed("Util",`Added [${result.title}](${fileUrl}) to queue.`, 'Added to Queue')]})
@@ -85,11 +128,13 @@ module.exports = {
 
         // Checks if we already have the audio downloaded. IF so then just play it rather than downloading again.
         if (fs.existsSync(filePath)) {
-            await cont()
+            await startPlayer()
         } else {
             ytdl(fileUrl, { filter: 'audioonly', format: 'highestaudio' }).pipe(fs.createWriteStream(filePath)).on("finish", async () => {
-                await cont()
+                await startPlayer()
             });
         }
+        //#endregion
+        */
     }
 }
