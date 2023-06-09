aea6358662036de8776a5cf15297b9907b4b534a
Ben Champagne <donkeykong797@gmail.com>
2023-06-09 12:50:08 -0400

    Added voice sense checks

--- src/util/voice.js
+++ src/util/voice.js
@@ -1,4 +1,5 @@
-const { CommandInteraction } = require("discord.js")
+const { CommandInteraction } = require("discord.js");
+const { consoleColors } = require("./consoleColors");
 
 module.exports = {
     /**
@@ -8,11 +9,20 @@ module.exports = {
     async canUseVoiceCommand(interaction) {
         const member = await interaction.guild.members.fetch(interaction.member.id);
         const voiceState = member?.voice
+        let canUse = true
 
-        if (!voiceState) {
-            await interaction.reply('You must be in a voice channel!')
-            return false
-        }
-        return true
+        if (!voiceState.channel || !voiceState?.guild?.id == interaction.guild.id) {
+            await interaction.deferReply({ephemeral: true})
+            await interaction.editReply('You must be in a voice channel!')
+            console.log(consoleColors.FG_RED+'User not in channel or guild')
+            canUse = false
+        } else if (!(interaction.guild.members.me.voice.channelId == voiceState.channelId)) {
+            await interaction.deferReply({ephemeral: true})
+            await interaction.editReply('You must be in the same voice channel as me!')
+            console.log(consoleColors.FG_RED+'User not in same channel')
+            canUse = false
+        } 
+        
+        return canUse
     }
 }
