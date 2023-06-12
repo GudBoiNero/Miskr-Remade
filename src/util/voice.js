0c818e1d9cdd044ac29535d179d176815e2920b9
Ben Champagne <donkeykong797@gmail.com>
2023-06-12 10:47:34 -0400

    Fixed bot not joining

--- src/util/voice.js
+++ src/util/voice.js
@@ -3,26 +3,47 @@ const { consoleColors } = require("./consoleColors");
 
 module.exports = {
     /**
-     * @param {CommandInteraction} interaction 
+     * @param {CommandInteraction} interaction
      * @returns {boolean}
      */
-    async canUseVoiceCommand(interaction) {
-        const member = await interaction.guild.members.fetch(interaction.member.id);
-        const voiceState = member?.voice
-        let canUse = true
+    async canUseVoiceCommand(
+        interaction,
+        requirements = {
+            inSameVoiceChannel: true,
+        }
+    ) {
+        const member = await interaction.guild.members.fetch(
+            interaction.member.id
+        );
+        const voiceState = member?.voice;
+        let canUse = true;
 
-        if (!voiceState.channel || !voiceState?.guild?.id == interaction.guild.id) {
-            await interaction.deferReply({ephemeral: true})
-            await interaction.editReply('You must be in a voice channel!')
-            console.log(consoleColors.FG_RED+'User not in channel or guild')
-            canUse = false
-        } else if (!(interaction.guild.members.me.voice.channelId == voiceState.channelId)) {
-            await interaction.deferReply({ephemeral: true})
-            await interaction.editReply('You must be in the same voice channel as me!')
-            console.log(consoleColors.FG_RED+'User not in same channel')
-            canUse = false
-        } 
-        
-        return canUse
-    }
-}
+        console.log(interaction.guild.members.me.voice.channel);
+        console.log(voiceState.channel);
+        if (
+            !voiceState.channel ||
+            !voiceState?.guild?.id == interaction.guild.id
+        ) {
+            await interaction.deferReply({ ephemeral: true });
+            await interaction.editReply("You must be in a voice channel!");
+            console.log(consoleColors.FG_RED + "User not in channel or guild");
+            canUse = false;
+        } else if (
+            !(
+                interaction.guild.members.me.voice.channelId ==
+                    voiceState.channelId ||
+                interaction.guild.members.me.voice.channel == null
+            ) &&
+            requirements.inSameVoiceChannel
+        ) {
+            await interaction.deferReply({ ephemeral: true });
+            await interaction.editReply(
+                "You must be in the same voice channel as me!"
+            );
+            console.log(consoleColors.FG_RED + "User not in same channel");
+            canUse = false;
+        }
+
+        return canUse;
+    },
+};
